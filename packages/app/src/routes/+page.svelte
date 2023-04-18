6d72705714c2be66366fd222c19d88f94f2d80c9
Bruno <seglinbruno@gmail.com>
2023-04-19 00:51:44 +0300

    Initialize socket client

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	import io from 'socket.io-client';
+
+	onMount(() => {
+		const socket = io('http://localhost:3000');
+	});
+</script>
