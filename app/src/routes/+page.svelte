cf4b5b72abeace76f0eaa6e153b0bee9aefc25e8
Bruno <seglinbruno@gmail.com>
2023-04-19 13:46:16 +0300

    Add wrapper to index page

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -34,21 +34,23 @@
 	};
 </script>
 
-<div class="flex flex-col gap-6 w-full max-w-[400px]">
-	<h2 class="text-3xl text-center font-bold mb-4">
-		{isAnswerFormVisible ? 'Tava Atbilde' : 'Tavs Vārds'}
-	</h2>
+<div class="flex flex-col items-center justify-center min-h-[100vh]">
+	<div class="flex flex-col gap-6 w-full max-w-[400px]">
+		<h2 class="text-3xl text-center font-bold mb-4">
+			{isAnswerFormVisible ? 'Tava Atbilde' : 'Tavs Vārds'}
+		</h2>
 
-	{#if isAnswerFormVisible}
-		<Input bind:value={answer} placeholder="Tava atbilde" large />
-		<Button on:click={submit} disabled={!answer || hasAnswered}>Iesūtīt</Button>
-		<Button
-			on:click={discard}
-			variant="destructive"
-			disabled={!answer || !hasAnswered || hasDiscarded}>Atmest</Button
-		>
-	{:else}
-		<Input bind:value={name} placeholder="Vārds Uzvārds" />
-		<Button on:click={showAnswerForm} disabled={!name}>Ienākt</Button>
-	{/if}
+		{#if isAnswerFormVisible}
+			<Input bind:value={answer} placeholder="Tava atbilde" large />
+			<Button on:click={submit} disabled={!answer || hasAnswered}>Iesūtīt</Button>
+			<Button
+				on:click={discard}
+				variant="destructive"
+				disabled={!answer || !hasAnswered || hasDiscarded}>Atmest</Button
+			>
+		{:else}
+			<Input bind:value={name} placeholder="Vārds Uzvārds" />
+			<Button on:click={showAnswerForm} disabled={!name}>Ienākt</Button>
+		{/if}
+	</div>
 </div>
