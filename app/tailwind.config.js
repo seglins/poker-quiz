72c6a21c680647455e719b9fa5584f7cdc40c8cd
Bruno <seglinbruno@gmail.com>
2023-04-19 03:40:53 +0300

    Add font, colors and base styles

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,9 +1,38 @@
+import plugin from 'tailwindcss/plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Inter', 'sans-serif']
+			},
+			colors: {
+				primary: '#2F80ED',
+				danger: '#EB5757',
+				neutral: {
+					light: '#C8C8C8',
+					dark: '#8E8E8E'
+				}
+			}
+		}
+	},
+	plugins: [
+		plugin(({ addBase, theme }) => {
+			addBase({
+				html: {
+					fontSize: '14px'
+				},
+				body: {
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					flexDirection: 'column',
+					minHeight: '100vh',
+					padding: `${theme('spacing.8')} ${theme('spacing.4')}`
+				}
+			});
+		})
+	]
+};
