7317fe9edf2001214d92f6b9468d27640da23882
Bruno <seglinbruno@gmail.com>
2023-04-19 03:41:09 +0300

    Add `Button` component

---
+++ app/src/lib/components/Button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let variant: 'primary' | 'destructive' = 'primary';
+	export let disabled: boolean = false;
+</script>
+
+<button
+	on:click
+	{disabled}
+	class="w-full px-4 py-4 text-white font-semibold rounded-lg"
+	class:bg-primary={variant === 'primary'}
+	class:bg-danger={variant === 'destructive'}
+	class:opacity-30={disabled}
+>
+	<slot /></button
+>
