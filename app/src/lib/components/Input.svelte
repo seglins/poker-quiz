0dd6db25fc44446a7ab93148f6b624bd74a10cd7
Bruno <seglinbruno@gmail.com>
2023-04-19 03:41:20 +0300

    Add `Input` component

---
+++ app/src/lib/components/Input.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let large: boolean = false;
+	export let disabled: boolean = false;
+	export let placeholder = '';
+	export let value = '';
+	export const largeClasses = 'text-2xl min-h-[100px] text-center';
+</script>
+
+<input
+	bind:value
+	{disabled}
+	{placeholder}
+	class={`w-full px-4 py-4 font-medium rounded-lg border border-neutral-light placeholder:text-neutral-dark ${
+		large && largeClasses
+	}`}
+/>
