503cc6b39044b4865a9178b468846b08b845b902
Bruno <seglinbruno@gmail.com>
2023-04-19 04:29:37 +0300

    Implement answer submitting functionality

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,8 +1,54 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
-	import io from 'socket.io-client';
+	import io, { Socket } from 'socket.io-client';
+	import { Event, type Player } from '@poker-quiz/lib/types';
+
+	import Button from '$lib/components/Button.svelte';
+	import Input from '$lib/components/Input.svelte';
+
+	let socket: Socket;
+
+	let name: string = '';
+	let answer: string = '';
+
+	let isAnswerFormVisible: boolean = false;
+	let hasAnswered: boolean = false;
+	let hasDiscarded: boolean = false;
+
+	$: player = { name, answer } as Player;
 
 	onMount(() => {
-		const socket = io('http://localhost:3000');
+		socket = io('http://localhost:3000');
 	});
+
+	const showAnswerForm = () => (isAnswerFormVisible = true);
+
+	const submit = () => {
+		socket.emit(Event.SUBMIT, player);
+		hasAnswered = true;
+	};
+
+	const discard = () => {
+		socket.emit(Event.DISCARD);
+		hasDiscarded = true;
+	};
 </script>
+
+<div class="flex flex-col gap-6 w-full max-w-[400px]">
+	<h2 class="text-3xl text-center font-bold mb-4">
+		{isAnswerFormVisible ? 'Tava Atbilde' : 'Tavs Vārds'}
+	</h2>
+
+	{#if isAnswerFormVisible}
+		<Input bind:value={answer} placeholder="Tava atbilde" large />
+		<Button on:click={submit} disabled={!answer || hasAnswered}>Iesūtīt</Button>
+		<Button
+			on:click={discard}
+			variant="destructive"
+			disabled={!answer || !hasAnswered || hasDiscarded}>Atmest</Button
+		>
+	{:else}
+		<Input bind:value={name} placeholder="Vārds Uzvārds" />
+		<Button on:click={showAnswerForm} disabled={!name}>Ienākt</Button>
+	{/if}
+</div>
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import { Event, Player, PlayerWithID } from '@poker-quiz/lib/types';
 
 const app = express();
 const server = http.createServer(app);
@@ -10,15 +11,36 @@ const io = new Server(server, {
 	},
 });
 
-const PORT = 3000;
+let players: PlayerWithID[] = [];
 
 io.on('connection', (socket) => {
-	console.log(`${socket.id} connected`);
+	const id = socket.id;
+
+	console.log(`${id} connected`);
+
+	socket.on(Event.SUBMIT, (player: Player) => {
+		players.push({ id, ...player });
+	});
+
+	socket.on(Event.DISCARD, () => {
+		players = players.map((player) => {
+			if (id === player.id) {
+				return {
+					...player,
+					hasDiscarded: true,
+				};
+			}
+
+			return player;
+		});
+	});
 
 	socket.on('disconnect', () => {
-		console.log(`${socket.id} disconnected`);
+		console.log(`${id} disconnected`);
 	});
 });
+
+const PORT = 3000;
 
 server.listen(PORT, () => {
 	console.log(`Running on ${PORT}`);
