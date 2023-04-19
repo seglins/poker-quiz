158040f3df83f520a3f3ac7caca54518e1202b0e
Bruno <seglinbruno@gmail.com>
2023-04-19 13:43:46 +0300

    Add `type` prop to `Input` component

--- app/src/lib/components/Input.svelte
+++ app/src/lib/components/Input.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
 	export let large: boolean = false;
 	export let disabled: boolean = false;
-	export let placeholder = '';
-	export let value = '';
+	export let placeholder: string = '';
+	export let value: string | number = '';
+	export let type: string = 'text';
+
 	export const largeClasses = 'text-2xl min-h-[100px] text-center';
+
+	const handleChange = (event: Event) => (value = (event.target as HTMLInputElement).value);
 </script>
 
 <input
-	bind:value
+	on:input={handleChange}
+	{value}
+	{type}
 	{disabled}
 	{placeholder}
-	class={`w-full px-4 py-4 font-medium rounded-lg border border-neutral-light placeholder:text-neutral-dark ${
+	class={`w-full px-4 py-4 font-medium rounded-lg border border-neutral placeholder:text-neutral-dark ${
 		large && largeClasses
 	}`}
 />
