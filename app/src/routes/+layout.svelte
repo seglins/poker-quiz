82fb79a6c61164c5585288de84470a212e2b5d85
Bruno <seglinbruno@gmail.com>
2023-04-22 16:26:54 +0300

    Move socket init to the main layout

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,5 +1,24 @@
 <script>
 	import '../app.css';
+	import { io } from 'socket.io-client';
+	import { page } from '$app/stores';
+	import { afterNavigate } from '$app/navigation';
+	import { PUBLIC_API_URL } from '$env/static/public';
+	import { socket } from '$lib/stores';
+
+	afterNavigate((navigation) => {
+		if ($socket || navigation.to?.route.id === '/admin/login') {
+			return;
+		}
+
+		socket.set(
+			io(PUBLIC_API_URL, {
+				query: {
+					token: $page.data.token
+				}
+			})
+		);
+	});
 </script>
 
 <main>
