d7553eefb20cf48ddd504d221403c24036f5b696
Bruno <seglinbruno@gmail.com>
2023-04-22 16:42:34 +0300

    Add admin player page

---
+++ app/src/routes/admin/[playerId]/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import type { IdentifiedPlayer } from '@poker-quiz/lib/types';
+	import { players } from '$lib/stores';
+
+	let player: IdentifiedPlayer | null;
+
+	onMount(() => {
+		players.subscribe((players) => {
+			const existingPlayer = players.find((player) => player.id === $page.params.playerId);
+
+			if (!existingPlayer) {
+				return;
+			}
+
+			player = existingPlayer;
+		});
+	});
+</script>
+
+{#if player}
+	<h3 class="text-xl font-bold mb-3">{player.name}</h3>
+	<p
+		class="text-4xl font-bold"
+		class:text-destructive={player.hasDiscarded}
+		class:opacity-50={player.hasDiscarded}
+	>
+		{player.answer}
+	</p>
+{/if}
