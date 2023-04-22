d7457b3050c4aa594af90841a85ebb6dda49fe25
Bruno <seglinbruno@gmail.com>
2023-04-22 16:17:50 +0300

    Update existing types and add new

--- lib/types.ts
+++ lib/types.ts
@@ -1,14 +1,25 @@
 export enum SocketEvent {
-	SUBMIT = 'submit',
-	DISCARD = 'discard',
+	PLAYER_SUBMIT = 'player:submit',
+	PLAYER_SUBMITTED = 'player:submitted',
+	PLAYER_DISCARD = 'player:discard',
+	PLAYER_DISCARDED = 'player:discarded',
+	REMOVE_PLAYERS = 'players:remove',
+	PLAYERS_REMOVED = 'players:removed',
+	PLAYER_REMOVED = 'player:removed',
+	GET_PLAYERS = 'players:get',
 }
+
+export type SocketEventGetPlayersResponse = {
+	players: IdentifiedPlayer[];
+};
 
 export interface Player {
 	name: string;
 	answer: string | number;
+	hasAnswered?: boolean;
 	hasDiscarded?: boolean;
 }
 
-export type PlayerWithID = {
+export type IdentifiedPlayer = {
 	id: string;
 } & Player;
